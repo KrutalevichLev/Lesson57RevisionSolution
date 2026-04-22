cc4ebdfaeea8a1b2052c008245da7d2331097487
KrutalevichLev <krutaleviclev@gmail.com>
2026-04-22 18:35:07 +0300

    task05 try

--- Task05/logic.h
+++ Task05/logic.h
@@ -4,4 +4,6 @@
 
 using namespace std;
 
-string get_all_local_maximums(int matrix[DEFAUL_SIZE][DEFAUL_SIZE], int n, int m);
+string get_last_local_maximums(int matrix[DEFAUL_SIZE][DEFAUL_SIZE], int n, int m);
+
+int get_local_maximums(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n, int m, int r, int c);
